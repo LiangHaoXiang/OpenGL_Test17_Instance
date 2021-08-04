57c05356a7f5143b92c9eae5bc28dc2a45808295
LiangHaoXiang <1013568397@qq.com>
2021-08-04 14:07:22 +0800

    Initial Commit

---
+++ OpenGL_Test17_Instance/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_Test17_Instance
+//
+//  Created by Haoxiang Liang on 2021/8/4.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
